9a287c2025ec846fbc692ffc00d7b35ffb3ea174
soumitya <soumityachauhan@gmail.com>
2024-06-25 02:54:17 +0100

    fix: added query and resolver for that query

--- gql-client-server/server/src/resolver.js
+++ gql-client-server/server/src/resolver.js
@@ -1,5 +1,12 @@
+import { DBBookData } from "./DBData.js";
+
 export const resolvers = {
   Query: {
     getHello: () => "Hello, world!",
+    books: () => DBBookData,
+    getBook: (id) =>
+      DBBookData.find((book) => {
+        book.id === id;
+      }),
   },
 };
